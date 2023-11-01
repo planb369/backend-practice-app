a95bd767ddb94b01159c0343f08c69fa77ebdeb8
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-01 18:56:29 +0900

    [add] データベースとの接続

--- index.ts
+++ index.ts
@@ -1,10 +1,36 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
+import * as mysql from "mysql"; // MySQLの型定義をインポート
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const db = mysql.createConnection({
+  host: dbHost,
+  port: Number(dbPort),
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+});
+
+console.log(dbHost);
+console.log(dbUser);
+console.log(dbPassword);
+console.log(dbName);
+
+// DB接続
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database: " + err.message);
+  } else {
+    console.log("Connected to database");
+  }
 });
 
 app.listen(port, () => {
