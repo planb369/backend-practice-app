0fbb07a814adecf831a46cdbc11e835cb403d075
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-02 16:34:14 +0900

    [add] noteのプロパティとインスタンスへのマッピング

--- models/noteModel.ts
+++ models/noteModel.ts
@@ -1,16 +1,41 @@
 import db from "../db";
 
+//データベースの一行分の定義
+type Row = {
+  id: number;
+  title: string;
+  content: string;
+};
+
 export class Note {
-  //データ一覧取得
+  //プロパティ
+  id: number;
+  title: string;
+  content: string;
+
+  // コンストラクターでプロパティの初期化
+  constructor() {
+    this.id = 0;
+    this.title = "";
+    this.content = "";
+  }
+
+  // データ一覧取得
   static search(): Promise<Note[]> {
     return new Promise((resolve, reject) => {
       db.query("SELECT * FROM notes", (error, results) => {
         if (error) {
-          //エラーならrejectを呼び出してエラー情報をPromiseに渡す
           reject(error);
         } else {
-          //成功ならresolveを呼び出して結果をPromiseに渡す
-          resolve(results);
+          //一行ずつインスタンスにマッピング
+          const notes: Note[] = results.map((row: Row) => {
+            const note = new Note();
+            note.id = row.id;
+            note.title = row.title;
+            note.content = row.content;
+            return note;
+          });
+          resolve(notes); //配列で返す
         }
       });
     });
