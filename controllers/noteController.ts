ff852d1ce56c2498ab3ebd64181c98e4a3e5dc6a
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-08 17:08:28 +0900

    [edit] ファイル名

--- controllers/noteController.ts
+++ controllers/noteController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { Note } from "../models/noteModel";
 import { NotFoundError } from "../errors/NotFoundError";
-import { HTTP_STATUS } from "../constants/httpStatus";
+import { HTTP_STATUS } from "../constants/httpStatusCodes";
 import { HTTP_STATUS_MESSAGE } from "../constants/httpStatusMessages";
 import { BadRequestError } from "../errors/BadRequestError";
 
