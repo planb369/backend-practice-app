546697954a16ca9112cc6bddae39474529de588b
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-15 14:03:05 +0900

    [edit] パスを後ろに設置してデータ挿入できた

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-app.use("/", routes);
-
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
