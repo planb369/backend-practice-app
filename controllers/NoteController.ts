d83b4754408d37917c6bf9829f8a8057d02aa546
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-22 10:32:51 +0900

    [edit] idの存在チェック変数の命名

--- controllers/NoteController.ts
+++ controllers/NoteController.ts
@@ -126,8 +126,8 @@ export class NoteController {
         throw new BadRequestError("idの値が不正です");
       }
 
-      const isExists = await NoteModel.findNote(id);
-      if (!isExists) {
+      const findNote = await NoteModel.findNote(id);
+      if (findNote === null) {
         throw new BadRequestError("存在しないidです");
       }
 
