b74b175d5afbaf6dea4aef360cca3369bc276983
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-22 11:59:33 +0900

    [add] idバリデーションと存在チェック

--- controllers/NoteController.ts
+++ controllers/NoteController.ts
@@ -158,9 +158,20 @@ export class NoteController {
 
   //削除
   async deleteNote(req: Request, res: Response) {
+    const id = req.params.id;
     try {
+      //idのバリデーション
+      if (!validationId(id)) {
+        throw new BadRequestError("idの値が不正です");
+      }
+
+      const note = await NoteModel.findNote(id);
+      if (!note) {
+        throw new BadRequestError("存在しないidです");
+      }
+
       //削除
-      const result = await NoteModel.deleteNote(req.params.id);
+      const result = await NoteModel.deleteNote(id);
       return res.status(HTTP_STATUS_CODES.OK).json(result);
     } catch (err) {
       if (err instanceof Error) {
