744c995f7a2435eb39d36aac1f3a635b9455f866
planb369 <146909711+planb369@users.noreply.github.com>
2023-11-29 12:25:10 +0900

    [delete]不要なもの

--- adapter/controller/NoteController.ts
+++ adapter/controller/NoteController.ts
@@ -3,22 +3,21 @@ import { GetNotesUseCase } from "../../application/usecase/GetNotesUseCase";
 import { GetNotesRequest } from "../transfer/request/GetNotesRequest";
 import { GetNotesResponse } from "../transfer/response/GetNotesResponse";
 import { handleErrors } from "./errors/handleErrors";
+import { HTTP_STATUS_CODES } from "./httpStatus/HTTP_STATUS_CODES";
 
 export class NoteController {
   getNotesUseCase = new GetNotesUseCase();
 
   async getNotes(req: Request, res: Response) {
     try {
+      //requestでバリデーション
       const request = new GetNotesRequest(
         parseInt(req.query.limit as string) || 50,
         parseInt(req.query.offset as string) || 0
       );
+      request.validate();
 
-      const validationError = request.validate();
-      if (validationError) {
-        return res.status(400).json({ error: validationError });
-      }
-
+      //ユースケースに渡す
       const output = await this.getNotesUseCase.getNotes(
         request.limit,
         request.offset
@@ -26,7 +25,7 @@ export class NoteController {
 
       //responseで整形して返却
       const response = new GetNotesResponse(output);
-      res.status(200).json(response.convertToJson());
+      res.status(HTTP_STATUS_CODES.OK).json(response.convertToJson());
     } catch (err) {
       if (err instanceof Error) {
         handleErrors(err, res);
