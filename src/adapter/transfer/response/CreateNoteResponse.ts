e74e6cda76d0d4c05bd7ac3d0ccafef1d2ff199a
planb369 <146909711+planb369@users.noreply.github.com>
2023-12-06 13:41:52 +0900

    [edit]返却の型

--- src/adapter/transfer/response/CreateNoteResponse.ts
+++ src/adapter/transfer/response/CreateNoteResponse.ts
@@ -1,17 +1,20 @@
+// response/CreateNoteResponse.ts
 import { CreateNoteOutput } from "../../../application/output/CreateNoteOutput";
 
 export class CreateNoteResponse {
-  readonly id?: string;
-  readonly title?: string;
-  readonly content?: string;
-  readonly createdAt?: string;
-  readonly updatedAt?: string;
+  readonly id;
+  readonly title;
+  readonly content;
+  readonly createdAt;
+  readonly updatedAt;
 
   constructor(output: CreateNoteOutput) {
-    this.id = output.item?.id?.value;
-    this.title = output.item?.title?.value;
-    this.content = output.item?.content?.value;
-    this.createdAt = output.item?.createdAt?.value;
-    this.updatedAt = output.item?.updatedAt?.value;
+    if (output.item) {
+      this.id = output.item.id?.value;
+      this.title = output.item.title;
+      this.content = output.item.content;
+      this.createdAt = output.item.createdAt;
+      this.updatedAt = output.item.updatedAt;
+    }
   }
 }
