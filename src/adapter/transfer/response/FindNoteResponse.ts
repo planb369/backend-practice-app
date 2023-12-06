c4d5eb754081395b815ae2973df69332fb6a6232
planb369 <146909711+planb369@users.noreply.github.com>
2023-12-06 16:04:42 +0900

    [fix]findの返り値

--- src/adapter/transfer/response/FindNoteResponse.ts
+++ src/adapter/transfer/response/FindNoteResponse.ts
@@ -1,17 +1,19 @@
 import { FindNoteOutput } from "../../../application/output/FindNoteOutput";
 
 export class FindNoteResponse {
-  readonly id?: string;
-  readonly title?: string;
-  readonly content?: string;
-  readonly createdAt?: string;
-  readonly updatedAt?: string;
+  readonly id;
+  readonly title;
+  readonly content;
+  readonly createdAt;
+  readonly updatedAt;
 
   constructor(output: FindNoteOutput) {
+    console.log(output); // デバッグ: 出力内容を確認
+
     this.id = output.item?.id?.value;
-    this.content = output.item?.content?.value;
-    this.title = output.item?.title?.value;
-    this.createdAt = output.item?.createdAt?.value;
-    this.updatedAt = output.item?.updatedAt?.value;
+    this.content = output.item?.content;
+    this.title = output.item?.title;
+    this.createdAt = output.item?.createdAt;
+    this.updatedAt = output.item?.updatedAt;
   }
 }
