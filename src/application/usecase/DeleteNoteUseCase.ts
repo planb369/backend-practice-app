9a70106080ee2c9f115fcb3f4aac8c4f38b5982e
planb369 <146909711+planb369@users.noreply.github.com>
2023-12-07 15:50:23 +0900

    [edit] データの存在チェック

--- src/application/usecase/DeleteNoteUseCase.ts
+++ src/application/usecase/DeleteNoteUseCase.ts
@@ -1,6 +1,7 @@
 import { DeleteNoteInput } from "../input/DeleteNoteInput";
 import { DeleteNoteOutput } from "../output/DeleteNoteOutput";
 import { NoteRepository } from "../../infrastructure/repository/NoteRepository";
+import { NotFoundError } from "../../adapter/controller/errors/NotFoundError";
 
 export class DeleteNoteUseCase {
   constructor(private readonly noteRepository: NoteRepository) {}
@@ -8,6 +9,12 @@ export class DeleteNoteUseCase {
   //関数は基本一つだけ
   async handle(input: DeleteNoteInput) {
     const note = input.getNote();
+
+    const targetNote = await this.noteRepository.find(note);
+    if (!targetNote) {
+      throw new NotFoundError("データが存在しません");
+    }
+
     const result = await this.noteRepository.delete(note);
     return new DeleteNoteOutput(result);
   }
--- src/application/usecase/UpdateNoteUseCase.ts
+++ src/application/usecase/UpdateNoteUseCase.ts
@@ -1,12 +1,20 @@
 import { UpdateNoteInput } from "../input/UpdateNoteInput";
 import { UpdateNoteOutput } from "../output/UpdateNoteOutput";
 import { NoteRepository } from "../../infrastructure/repository/NoteRepository";
+import { NotFoundError } from "../../adapter/controller/errors/NotFoundError";
 
 export class UpdateNoteUseCase {
   constructor(private readonly noteRepository: NoteRepository) {}
 
   async handle(input: UpdateNoteInput): Promise<UpdateNoteOutput> {
     const note = input.getNote();
+    console.log(note);
+
+    const targetNote = await this.noteRepository.find(note);
+    if (!targetNote) {
+      throw new NotFoundError("データが存在しません");
+    }
+
     const result = await this.noteRepository.update(note);
 
     return new UpdateNoteOutput(result);
